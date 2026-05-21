69f33bb16bc0933af622476788ffcf6e1c82820d
25740-Emily <25740@burnside.school.nz>
2026-05-22 09:25:54 +1200

    Player and platform

--- scripts/hamburger(player).gd
+++ scripts/hamburger(player).gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var start_position = Vector2(-32,296)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,3 +22,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	# handle repawn
+	if position.y > 900:
+		#repawn
+		position = start_position
