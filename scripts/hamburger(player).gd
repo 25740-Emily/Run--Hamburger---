afad029945013d0dd7d0cb8925385c7efa21e093
25740-Emily <25740@burnside.school.nz>
2026-05-28 14:41:49 +1200

    double jumps (need to debug)

--- scripts/hamburger(player).gd
+++ scripts/hamburger(player).gd
@@ -4,14 +4,24 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var start_position = Vector2(-32,296)
 
-func _physics_process(delta: float) -> void:
+@export var MAX_JUMPS = 2
+var jump_count = 0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		else:
+			# Reset jump count when landing on the ground
+			jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or jump_count < MAX_JUMPS:
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
