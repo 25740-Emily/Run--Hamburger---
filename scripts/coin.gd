02d108aeb273faa18c6e508437a2d453c4774a73
25740-Emily <25740@burnside.school.nz>
2026-05-29 12:36:16 +1200

    portal(fountain) & coin

---
+++ scripts/coin.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	# If the object touching the coin is named "Hamburger(player)", collect it 
+	if body.name == "Hamburger(player)":
+		print("Coin collected! +1 Score")
+		queue_free() # Destroys the coin instantly
