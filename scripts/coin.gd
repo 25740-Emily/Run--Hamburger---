0e5f7e8d015d347332547e238867804cbac2aefb
25740-Emily <25740@burnside.school.nz>
2026-05-29 19:16:36 +1200

    fixed portal bug

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -6,4 +6,4 @@ func _on_body_entered(body: Node2D) -> void:
 	# If the object touching the coin is named "Hamburger(player)", collect it 
 	if body.name == "Hamburger(player)":
 		print("Coin collected! +1 Score")
-		queue_free() # Destroys the coin instantly
+		call_deferred("queue_free")# Destroys the coin instantly
--- scripts/portal(fountain).gd
+++ scripts/portal(fountain).gd
@@ -3,7 +3,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Hamburger(player)":
-		print("you won")
+		print("Next Leve!")
 		#load a new level
-		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
-		pass
+		get_tree().call_deferred("change_scene_to_file","res://scenes/level_2.tscn")
