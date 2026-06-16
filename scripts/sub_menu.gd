8c03fc1a93c0ff324e8cf410ff76b23ea46b6590
25740-Emily <25740@burnside.school.nz>
2026-06-17 10:05:00 +1200

    restart button

--- scripts/sub_menu.gd
+++ scripts/sub_menu.gd
@@ -2,9 +2,7 @@ extends Node2D
 
 
 func _on_restart_pressed() -> void:
-	get_tree().paused = false
-	get_tree().reload_current_scene()
-
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 	pass # Replace with function body.
 
 
