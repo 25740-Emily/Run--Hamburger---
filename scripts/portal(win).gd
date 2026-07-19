95fa4135e6f26efc356bd0dba74b1e225faa7d79
25740-Emily <25740@burnside.school.nz>
2026-07-20 00:33:40 +1200

    player changed

--- scripts/portal(win).gd
+++ scripts/portal(win).gd
@@ -4,7 +4,6 @@ extends Area2D
 var win_scene_path = "res://scenes/win.tscn"
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("take_damage"):
+	if body.name == "player2" or body.name == "Hamburger(player)":
 		print("YOU WIN!")
 		get_tree().call_deferred("change_scene_to_file", win_scene_path)
-	pass # Replace with function body.
